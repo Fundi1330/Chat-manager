edf1cc7971fdb40acef8f9da48442c709a9bbaf3
Fundi1330 <88210377+Fundi1330@users.noreply.github.com>
2023-05-01 15:53:23 +0300

    change: config order & prefix perms and config

--- config.js
+++ config.js
@@ -3,40 +3,55 @@
 
 const config = new JsonConfigFile('./plugins/chatmanager/config.json');
 
+// auto moderation
 config.init('auto-mod', {
     'banwords': true,
     'anti-caps': true,
     'messageLenghtLimit': true,
     'anti-spam': true
-})
+});
+// banwords
 config.init('banwordsList', []);
 config.init('banwordMessage', 'your message contains banword');
+// caps
 config.init('capsPercentage', 0.5);
+config.init('capsMessage', 'your message have too many caps');
+// spam
 config.init('spamRate', 5);
 config.init('spamPer', 8);
-config.init('capsMessage', 'your message have too many caps');
-config.init('radius', 100);
+config.init('spamMessage', "Don't spam!")
+// message lenght limit
+config.init('messageLenghtLimit', 200);
+config.init('messageLenght', "Your message lenght is too high!");
+// chat
+config.init('localMessageRadius', 100);
 config.init('globalPrefix', '[G]');
 config.init('globalSymbol', '!');
 config.init('localPrefix', '[L]');
+// prefixes
 config.init('prefix', true);
+config.init('prefixPermName', 'admin');
+config.init('changeNoPermMessage', "You don't have permission for change this player's prefix");
+config.init('changePlayerPrefixMessage', 'You succefully added prefix to player');
+config.init('resetNoPermMessage', "You don't have permission for reset this player's prefix");
+config.init('resetPlayerPrefixMessage', "You succefully removed player's prefix");
+// chat bot
 config.init('chatBotPrefix', '[ChatBot]');
 config.init('chatBotMessage', 'this is chat bot message');
 config.init('chatBotTimeBetweenMessages', 300000); // 5 minutes
+// logs
 config.init('logs', {
     'chat': false,
     'commands': false
 });
+// commands
 config.init('commands', {
     'mute': true
 });
-config.init('messageLenghtLimit', 200);
 config.init('playerMutedMessage', "You've been muted!");
 config.init('mutePlayerMessage', 'Succefully muted player!');
 config.init('playerUnmutedMessage', "You've been unmuted!");
 config.init('unmutePlayerMessage', 'Succefully unmuted player!');
-config.init('messageLenght', "Your message lenght is too high!");
-config.init('spamMessage', "Don't spam!")
 
 config.close();
 
--- prefixes.js
+++ prefixes.js
@@ -1,9 +1,14 @@
-//LiteLoaderScript Dev Helper
-/// <reference path="c:\Users\LenovoG580\Documents\ll\dts/dts/HelperLib-master/src/index.d.ts"/> 
+// LiteLoader-AIDS automatic generated
+/// <reference path="c:\Users\LenovoG580\Documents\ll/dts/HelperLib-master/src/index.d.ts"/> 
 
 const prefixDB = new KVDatabase('./plugins/chatmanager/prefix');
 const config = require('./config.js');
 const prefix = config.get('prefix');
+const prefixPerm = config.get('prefixPermName');
+const changeNoPermMessage = config.get('changeNoPermMessage');
+const changePlayerPrefixMessage = config.get('changePlayerPrefixMessage');
+const resetNoPermMessage = config.get('resetNoPermMessage');
+const resetPlayerPrefixMessage = config.get('resetPlayerPrefixMessage');
 
 mc.listen('onServerStarted', () => {
     if (prefix) {
@@ -20,22 +25,22 @@ mc.listen('onServerStarted', () => {
         
         reset.setCallback((_change, ori, out, res) => {
             const pl = res.player[0];
-            if (res.player != undefined && ori.player.hasTag('king')) {
+            if (res.player != undefined && ori.player.hasTag(prefixPerm)) {
                 prefixDB.set(pl.realName, '');
                 pl.rename(pl.realName);
-                return out.success('You succefully removed player prefix');
+                return out.success(resetPlayerPrefixMessage);
             }
-            return out.error("You don't have permission for this");
+            return out.error(resetNoPermMessage);
         });
 
         change.setCallback((_reset, ori, out, res) => {
             const pl = res.player[0];
-            if (res.player != undefined && ori.player.hasTag('king')) {
+            if (res.player != undefined && ori.player.hasTag(prefixPerm)) {
                 prefixDB.set(pl.realName, `${res.color}[${res.prefix}]`);
                 pl.rename(`${res.color}[${res.prefix}] §r` + pl.realName);
-                return out.success('You succefully added prefix to player');
+                return out.success(changePlayerPrefixMessage);
             }
-            return out.error("You don't have permission for this");
+            return out.error(changeNoPermMessage);
         });
         reset.setup();
         change.setup();
@@ -45,9 +50,9 @@ mc.listen('onServerStarted', () => {
 mc.listen('onJoin', (pl) => {
     if (prefix) {
         const prefix = prefixDB.get(pl.realName);
-    if (prefix != undefined && pl.name != prefix) {
-        pl.rename(`${prefix}§r` + pl.realName)
-    }
+        if (prefix != undefined && pl.name != prefix) {
+            pl.rename(`${prefix}§r` + pl.realName)
+        }
     }
 })
 
